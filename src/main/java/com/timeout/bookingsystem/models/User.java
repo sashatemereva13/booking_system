f84136074edde89b27fb142ac2bff481a649b0d3
irinakiseleva0 <irina.kiseleva@epita.fr>
2025-12-16 10:24:19 +0100

    fix users

--- src/main/java/com/timeout/bookingsystem/models/User.java
+++ src/main/java/com/timeout/bookingsystem/models/User.java
@@ -11,10 +11,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "firstname", nullable = false)
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @Column(name = "lastname", nullable = false)
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 
     @Column(name = "address", nullable = false)
